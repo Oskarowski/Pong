11cfc65160cd0c323dbc50b81d488fa2a314116c
Oskarowski <oscar.kacprzak@gmail.com>
2023-01-01 13:58:22 +0100

    ball, player, paddle as classes

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 console.log("main.js loaded");
 
 const canvas = document.getElementById("gameCanvas");
-const clearButton = document.getElementById("clearButton");
+const pauseButton = document.getElementById("pauseButton");
 
 const ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 
-const STATE_CHANGE_INTERVAL = 10;
+const STATE_CHANGE_INTERVAL = 7;
 
 const CANVAS_HEIGHT = canvas.height;
 const CANVAS_WIDTH = canvas.width;
@@ -28,62 +28,6 @@ const BALL_START_Y = CANVAS_HEIGHT / 2;
 const BALL_SPEED_VX = 4.5;
 const BALL_SPEED_VY = 1.5;
 
-const ball = {
-    r: BALL_R,
-    x: BALL_START_X,
-    y: BALL_START_Y,
-    vx: BALL_SPEED_VX,
-    vy: BALL_SPEED_VY,
-
-    move: function() {
-        this.x += this.vx;
-        this.y += this.vy;
-    },
-    moveToCentre: function() {
-        this.x = BALL_START_X;
-        this.y = BALL_START_Y;
-    },
-    outOnLeft: function() {
-        if(this.x + BALL_R <= 0) {
-            p2Points++;
-            return true; 
-        }
-        return false;
-    },
-    outOnRight: function() {
-        if(this.x - BALL_R >= CANVAS_WIDTH) {
-            p1Points++;
-            return true;
-        }
-        return false;
-    },
-    bounceFromWall: function() {
-        this.vy = -this.vy;
-    },
-    bounceFromPaddle: function() {
-        this.vx = -this.vx;
-    },
-    shouldBounceFromTopWall: function() {
-        return this.y - BALL_R <= 0 && this.vy < 0;
-    },
-    shouldBounceFromBottomWall: function() {
-        return this.y + BALL_R >= CANVAS_HEIGHT && this.vy > 0;
-    },
-    isOnTheSameHeightAsPaddle: function(paddleY) {
-        return isInPaddleRange(this.y, paddleY, paddleY + PADDLE_HEIGHT)
-    },
-    shouldBounceFromLeftPaddle: function() {
-        return this.vx < 0 && 
-        isInPaddleRange(this.x - BALL_R, PADDLE_P1_X, PADDLE_P1_X + PADDLE_WIDTH) &&
-        ball.isOnTheSameHeightAsPaddle(p1PaddleY);
-    },
-    shouldBounceFromRightPaddle: function() {
-        return this.vx > 0 && 
-        isInPaddleRange(this.x + BALL_R, PADDLE_P2_X, PADDLE_P2_X + PADDLE_WIDTH) &&
-        ball.isOnTheSameHeightAsPaddle(p2PaddleY);
-    },
-}
-
 const P1_UP_BUTTON = "KeyW";
 const P1_DOWN_BUTTON = "KeyS";
 const P2_UP_BUTTON = "ArrowUp";
@@ -95,53 +39,136 @@ const STOP_ACTION = "stop";
 
 const PAUSE_BUTTON = "KeyP";
 
-let p1PaddleY  = PADDLE_START_Y;
-let p2PaddleY = PADDLE_START_Y;
-let p1Points = 0;
-let p2Points = 0;
+setInterval(updateAndChangeState, STATE_CHANGE_INTERVAL);
 
-let p1Action = STOP_ACTION;
-let p2Action = STOP_ACTION;
+class Ball {
+    constructor(){
+        this.r = BALL_R;
+        this.x = BALL_START_X;
+        this.y = BALL_START_Y;
+        this.vx = BALL_SPEED_VX;
+        this.vy = BALL_SPEED_VY;
+    }
+
+    move() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+    moveToCentre() {
+        this.x = BALL_START_X;
+        this.y = BALL_START_Y;
+    }
+    outOnLeft() {
+        if(this.x + BALL_R <= 0) {
+            p2.points++;
+            return true; 
+        }
+        return false;
+    }
+    outOnRight() {
+        if(this.x - BALL_R >= CANVAS_WIDTH) {
+            p1.points++;
+            return true;
+        }
+        return false;
+    }
+    bounceFromWall() {
+        this.vy = -this.vy;
+    }
+    bounceFromPaddle() {
+        this.vx = -this.vx;
+    }
+    shouldBounceFromTopWall() {
+        return this.y - BALL_R <= 0 && this.vy < 0;
+    }
+    shouldBounceFromBottomWall() {
+        return this.y + BALL_R >= CANVAS_HEIGHT && this.vy > 0;
+    }
+    isOnTheSameHeightAsPaddle(paddleY) {
+        return isInPaddleRange(this.y, paddleY, paddleY + PADDLE_HEIGHT)
+    }
+    shouldBounceFromLeftPaddle() {
+        return this.vx < 0 && 
+        isInPaddleRange(this.x - BALL_R, PADDLE_P1_X, PADDLE_P1_X + PADDLE_WIDTH) &&
+        ball.isOnTheSameHeightAsPaddle(p1.paddle.y);
+    }
+    shouldBounceFromRightPaddle() {
+        return this.vx > 0 && 
+        isInPaddleRange(this.x + BALL_R, PADDLE_P2_X, PADDLE_P2_X + PADDLE_WIDTH) &&
+        ball.isOnTheSameHeightAsPaddle(p2.paddle.y);
+    }
+    drawYourself(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+class Paddle {
+    constructor(paddleX){
+        this.x = paddleX;
+        this.y = PADDLE_START_Y;
+    }
+    setY(newY){
+        const min = 0;
+        const max = CANVAS_HEIGHT - PADDLE_HEIGHT;
+        
+        this.y = inRange(newY, min, max); 
+    }
+    moveDown() {
+        this.setY(this.y + PADDLE_STEP);
+    }
+    moveUp() {
+        this.setY(this.y - PADDLE_STEP);
+    }
+    drawYourself(){
+        ctx.fillRect(this.x, this.y, PADDLE_WIDTH, PADDLE_HEIGHT);
+    }
+}
+
+class Player {
+    constructor(paddleX, boardX){
+        this.points = 0;
+        this.board = boardX;
+        this.action = STOP_ACTION;
+        this.paddle = new Paddle(paddleX);
+    }
+    moveAction(){
+        if(this.action === UP_ACTION){
+            // console.log("this.paddle.moveUp()");
+            this.paddle.moveUp();
+        }
+        else if(this.action === DOWN_ACTION){
+            // console.log("this.paddle.moveDown()");
+            this.paddle.moveDown();
+        }
+    }
+}
+
+
+const ball = new Ball();
+const p1 = new Player(PADDLE_P1_X, BOARD_P1);
+const p2 = new Player(PADDLE_P2_X, BOARD_P2);
 
 let isPaused = false;
 
-function drawState() {
-    clearCanvas();
-    drawPoints(p1Points.toString(), BOARD_P1, BOARD_Y);
-    drawPoints(p2Points.toString(), BOARD_P2, BOARD_Y);
-    drawBall(ball.x, ball.y, ball.r);
-    drawPaddle(PADDLE_P1_X, p1PaddleY);
-    drawPaddle(PADDLE_P2_X, p2PaddleY);
-}
-
-function drawPaddle(x,y) {
-    ctx.fillRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
-}
 function drawPoints(text, x, y) {
     if(typeof(y)===undefined) y=10;
     ctx.fillText(text,x,y);
 }
-function drawBall(x,y,r) {
-    ctx.beginPath();
-    ctx.arc(x,y,r,0,Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill();
-}
 
 function clearCanvas() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
-function updateAndChangeState() {
-    if(isPaused) return;
-    updateState();
-    drawState();
-}
-
-function updateState() {
-    movePaddles();
-    moveBall();    
+function drawState() {
+    clearCanvas();
+    drawPoints(p1.points.toString(), p1.board, BOARD_Y);
+    drawPoints(p2.points.toString(), p2.board, BOARD_Y);
+    ball.drawYourself();
+    p1.paddle.drawYourself();
+    p2.paddle.drawYourself();
 }
 
 function moveBall() {
@@ -151,58 +178,63 @@ function moveBall() {
     ball.move();
 }
 
-setInterval(updateAndChangeState, STATE_CHANGE_INTERVAL);
+function updateState() {
+    p1.moveAction();
+    p2.moveAction();
+    moveBall();    
+}
+
+function updateAndChangeState() {
+    if(isPaused) return;
+    drawState();
+    updateState();
+}
+
+
 
 window.addEventListener("keydown", event => {
     const code = event.code;
     console.log("keydown " + code);
     if(code === P1_UP_BUTTON){
-        p1Action = UP_ACTION;
+        p1.action = UP_ACTION;
     }
     if(code === P1_DOWN_BUTTON){
-        p1Action = DOWN_ACTION;
+        p1.action = DOWN_ACTION;
     }
-
+    
     if(code === P2_UP_BUTTON){
-        p2Action = UP_ACTION;
+        p2.action = UP_ACTION;
     }
     if(code === P2_DOWN_BUTTON){
-        p2Action = DOWN_ACTION;
+        p2.action = DOWN_ACTION;
     }
 });
+
 window.addEventListener("keyup", event => {
     const code = event.code;
     console.log("keyup " + code);
-    if(code === P1_UP_BUTTON && p1Action === UP_ACTION || code === P1_DOWN_BUTTON && p1Action === DOWN_ACTION) { p1Action = STOP_ACTION; }
-    if(code === P2_UP_BUTTON && p2Action === UP_ACTION || code === P2_DOWN_BUTTON && p2Action === DOWN_ACTION) { p2Action = STOP_ACTION; }
+    if(code === P1_UP_BUTTON && p1.action === UP_ACTION || code === P1_DOWN_BUTTON && p1.action === DOWN_ACTION) { p1.action = STOP_ACTION; }
+    if(code === P2_UP_BUTTON && p2.action === UP_ACTION || code === P2_DOWN_BUTTON && p2.action === DOWN_ACTION) { p2.action = STOP_ACTION; }
 });
+
 window.addEventListener("keydown", event => {
     const code = event.code;
     if(code === PAUSE_BUTTON){ isPaused = !isPaused; }
 });
 
-function movePaddles(){
-    if(p1Action === UP_ACTION && p1PaddleY >= 0){
-        p1PaddleY -= PADDLE_STEP;
-    }
-    else if(p1Action === DOWN_ACTION && p1PaddleY <= CANVAS_HEIGHT-PADDLE_HEIGHT){
-        p1PaddleY += PADDLE_STEP;
-    }
 
-    if(p2Action === UP_ACTION && p2PaddleY >= 0){
-        p2PaddleY -= PADDLE_STEP;
-    }
-    else if(p2Action === DOWN_ACTION && p2PaddleY <= CANVAS_HEIGHT-PADDLE_HEIGHT){
-        p2PaddleY += PADDLE_STEP;
-    }
+pauseButton.addEventListener("click", (event) => {
+    console.log("PAUSE button clicked");
+    isPaused = !isPaused;
+});
+
+function inRange(value, min, max){
+    console.log("inRange(value, min, max)");
+    if (value <= min) { return min; }
+    else if (value >= max) {return max; }
+    return value;
 }
 
 function isInPaddleRange(yPos, min, max){
     return yPos >= min && yPos <= max;
 }
-
-clearButton.addEventListener("click", (event) => {
-    console.log("Clear button clicked");
-    clearCanvas();
-});
-    
